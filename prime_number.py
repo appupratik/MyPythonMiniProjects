d9658967d7d8f926271b4e94a6e96a17eef98eb4
Pratik Bhatt <50225639+appupratik@users.noreply.github.com>
2020-10-06 16:33:25 +0530

    prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -9,14 +9,19 @@ result = []
 
 #if the both entered value is integer then string is run else 
 if num.isdigit() and num1.isdigit():
-    for r in range(int(num),int(num1)):
-        if r > 0:  
-           for i in range(2,r):  
-               if (r % i) == 0:    
-                   break  
-           else:  
-               result.append(r)
-    print(result)
+#check the entered values has difference or not
+    if int(num)<int(num1):
+        for r in range(int(num),int(num1)+1):
+            if r > 0:  
+                for i in range(2,r):  
+                    if (r % i) == 0:    
+                        break  
+                else:  
+                    result.append(r)
+        print(result)
+#the first value is greater than second value then gives this message        
+    else:
+        print("Second Value is Lowerthan First Value")
 else:
     print("\nEnter Numbers only")
 
